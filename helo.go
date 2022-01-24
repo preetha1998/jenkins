2109360544998abf4c7e8d645ad5790af0b2951e
preetha1998 <60173739+preetha1998@users.noreply.github.com>
2022-01-24 19:42:31 +0530

    statement

---
+++ helo.go
@@ -0,0 +1,5 @@
+package main
+import "fmt"
+func main() {
+fmt.Println("This is comes from github and connected with jenkins it will automatically build, test and release in production environment")
+}
